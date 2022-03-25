d0c14738985bde130185f1fa51614fec356cc856
EiZeiHei <lengao7@outlook.com>
2022-03-25 15:33:05 +0800

    Chapter3

---
+++ Chapter3/Homework04.java
@@ -0,0 +1,21 @@
+/**
+ * Chapter3
+ * Homework04.java
+ * 输出信息
+ */
+
+public class Homework04
+{
+	public static void main(String args[])
+	{
+		String name = "LA";
+		int age = 22;
+		int score = 100;
+		char gender = '男';
+		String hobby = "睡觉";
+
+		System.out.println("姓名\t年龄\t成绩\t性别\t爱好\n" + 
+			name + "\t" + age + "\t" + score + "\t" +
+			gender + "\t" + hobby);
+	}
+} 
